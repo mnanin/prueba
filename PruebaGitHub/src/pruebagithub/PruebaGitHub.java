8d3323026aa39e6073fef49466df0ff8619d5f9b
mnanin92 <mnanmar72@insgabrielamistral.cat>
2019-11-28 16:10:50 +0100

    Segundo cambio

--- PruebaGitHub/src/pruebagithub/PruebaGitHub.java
+++ PruebaGitHub/src/pruebagithub/PruebaGitHub.java
@@ -6,6 +6,7 @@ public class PruebaGitHub {
     public static void main(String[] args) {
 
         System.out.println("¡Hola mundo!");
+        System.out.println("Otra modificación");
 
     }
     
